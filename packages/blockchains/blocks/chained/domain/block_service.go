02031348460ef716d4ecaa91bbbff79fcbf20efb
Steve Rodrigue <steve@irestful.net>
2018-03-02 05:51:10 -0500

    Added the chained block repository and service interfaces

---
+++ packages/blockchains/blocks/chained/domain/block_service.go
@@ -0,0 +1,10 @@
+package domain
+
+import (
+	stored_chained_blocks "github.com/XMNBlockchain/core/packages/storages/blocks/chained/domain"
+)
+
+// BlockService represents a block service
+type BlockService interface {
+	Save(blk Block) (stored_chained_blocks.Block, error)
+}
